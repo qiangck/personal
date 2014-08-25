87055bb4a66e35d5e29c23a44531f27fdf4d4bf1
rechie1985 <lsl1276@gmail.com>
2014-08-25 16:25:49 +0800

    增加完成后取消事件的机制

--- core/RC.js
+++ core/RC.js
@@ -20,7 +20,6 @@ define(function (){
     RC.Slide = function(opt){
         var that = this;
         that.isSlide = false;
-        that.currentTarget = null;
         that.touchIdentifier = null;
         // 配置文件
         that.opt = RC.merge({
@@ -30,62 +29,103 @@ define(function (){
             slideMove: null
         },opt);
         var elemQuery = 'body';
-        var elems = [].slice.call(document.querySelectorAll(elemQuery));
-        elems.forEach(function(obj, index) {
-            obj.addEventListener(RC.EVENT.TOUCH_START, function(e) {
-                e.preventDefault();
-                var touches = e.touches;
-                if(that.touchIdentifier == null) {
-                    that.touchIdentifier = touches[0].identifier;
-                } else {
-                    [].slice.apply(touches).forEach(function(touch, index) {
-                    });
+        this.elems = [].slice.call(document.querySelectorAll(elemQuery));
+        that.startHandler = function(e){
+            return that.StartHandler.apply(that, arguments);
+        }
+        that.moveHandler = function(e){
+            return that.MoveHandler.apply(that, arguments);
+        }
+        that.endHandler = function(e){
+            return that.EndHandler.apply(that, arguments);
+        }
+        this.elems.forEach(function(obj, index) {
+            obj.addEventListener(RC.EVENT.TOUCH_START, that.startHandler);
+            obj.addEventListener(RC.EVENT.TOUCH_MOVE, that.moveHandler);
+            obj.addEventListener(RC.EVENT.TOUCH_END, that.endHandler);
+        });
+    };
+
+    RC.Slide.prototype.cancel = function() {
+        var that = this;
+        this.elems.forEach(function(obj, index){
+            obj.removeEventListener(RC.EVENT.TOUCH_START, that.startHandler, false);
+            obj.removeEventListener(RC.EVENT.TOUCH_MOVE, that.moveHandler, false);
+            obj.removeEventListener(RC.EVENT.TOUCH_END, that.endHandler, false);
+        });
+    };
+
+    /**
+     * touch start事件监听
+     * @param e
+     * @constructor
+     */
+    RC.Slide.prototype.StartHandler = function(e) {
+        e.preventDefault();
+        var that = this;
+        var touches = e.touches;
+        if(this.touchIdentifier == null) {
+            this.touchIdentifier = touches[0].identifier;
+        } else {
+            [].slice.apply(touches).forEach(function(touch, index) {
+            });
+        }
+        if(e.touches.length > 1) {
+            // 增加点触摸时，不处理
+            return;
+        }
+        if(this.isSlide ===  false) {
+            this.isSlide = true;
+            if(typeof this.opt.slideBegin === 'function') {
+                that.opt.slideBegin.call(that, RC.merge({}, touches[0]));
+            }
+        }
+    };
+
+    /**
+     * touch move事件监听
+     * @param e
+     * @constructor
+     */
+    RC.Slide.prototype.MoveHandler = function(e) {
+        var that = this;
+        e.preventDefault();
+        var touches = (e.targetTouches.length) ? e.targetTouches : e.changedTouches;
+        if(that.isSlide = true) {
+            // 判断是否是触发slide的touch
+            [].slice.apply(touches).forEach(function(touch, index) {
+                if(touch.identifier == that.touchIdentifier && typeof that.opt.slideMove === 'function') {
+                    that.opt.slideMove.call(that, RC.merge({}, touch));
                 }
-                if(e.touches.length > 1) {
-                    // 增加点触摸时，不处理
-                    return;
-                }
-                if(that.isSlide ===  false) {
-                    that.isSlide = true;
-                    that.currentTarget = obj;
-                    if(typeof that.opt.slideBegin === 'function') {
-                        that.opt.slideBegin(RC.merge({}, touches[0]));
+            });
+        }
+    };
+
+    /**
+     * touch end事件监听
+     * @param e
+     * @constructor
+     */
+    RC.Slide.prototype.EndHandler = function(e) {
+        var that = this;
+        e.preventDefault();
+        var touches = e.changedTouches;
+        if(that.isSlide === true) {
+            // 判断是否是触发slide的touch
+            [].slice.apply(touches).forEach(function(touch, index) {
+                if(touch.identifier == that.touchIdentifier) {
+                    that.isSlide = false;
+                    that.touchIdentifier = null;
+                    if(typeof that.opt.slideEnd === 'function') {
+                        var bCancel = that.opt.slideEnd.call(that, RC.merge({}, touch));
+                        if(bCancel == true) {
+                            that.cancel();
+                        }
                     }
                 }
             });
-            obj.addEventListener(RC.EVENT.TOUCH_MOVE, function(e) {
-                e.preventDefault();
-                var touches = (e.targetTouches.length) ? e.targetTouches : e.changedTouches;
-                if(that.isSlide = true) {
-                    // 判断是否是触发slide的touch
-                    [].slice.apply(touches).forEach(function(touch, index) {
-                        if(touch.identifier == that.touchIdentifier && typeof that.opt.slideMove === 'function') {
-                            that.opt.slideMove(RC.merge({}, touch));
-                        }
-                    });
-                }
-            });
-            obj.addEventListener(RC.EVENT.TOUCH_END, function(e) {
-                e.preventDefault();
-                var touches = e.changedTouches;
-                if(that.isSlide === true) {
-                    // 判断是否是触发slide的touch
-                    [].slice.apply(touches).forEach(function(touch, index) {
-                        if(touch.identifier == that.touchIdentifier) {
-                            that.isSlide = false;
-                            that.currentTarget = null;
-                            that.touchIdentifier = null;
-                            if(typeof that.opt.slideEnd === 'function') {
-                                that.opt.slideEnd(RC.merge({}, touch));
-                            }
-                        }
-                    });
-                }
-            });
-        });
+        }
     };
-    RC.Slide.prototype.showArrow = function() {
 
-    };
     return RC;
 });
--- slide/main.js
+++ slide/main.js
@@ -34,11 +34,14 @@ define(['../core/RC', '../util/MathUtil', '../util/ElemUtil', 'Game'], function 
                 startPoint = null;
                 endPoint = null;
                 game.drawArrow(0, 0, 0, 0);
+                // 返回true 取消事件监听
+                // return true;
             } catch(e) {
                 alert('end error: ' + e.message);
             }
         }
     });
+
 
     function init() {
         try{
