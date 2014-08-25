1ab67931257c0578bb29e9e8e4a3fb5e21b0d372
rechie1985 <lsl1276@gmail.com>
2014-08-25 11:08:49 +0800

    完成canvas中滑动显示箭头的功能
    1、利用canvas的rotate和translate函数来解决按照固定点旋转图像的问题

--- slide/Game.js
+++ slide/Game.js
@@ -1,40 +1,64 @@
 /**
  * Created by rechie on 14-8-21.
  */
-define(function() {
-   var Game = function(id){
-       this.canvasElem = document.getElementById('gameScreen');
-       this.ctx = this.canvasElem.getContext('2d');
-       this.resize();
-   };
-   Game.prototype.init = function() {
+define( function() {
+    var Game = function(id){
+        this.canvasElem = document.getElementById('gameScreen');
+        this.ctx = this.canvasElem.getContext('2d');
+        this.init();
+    };
+    Game.prototype.init = function() {
+        this.resize();
+        this.draw();
+    };
+    Game.prototype.draw = function () {
+        var bgImg = new Image();
+        bgImg.src = 'images/tfz.jpg';
+        var imgScale = 288 / 455;
+        var winScale = this.ctx.width / this.ctx.height;
+        var nowWidth, nowHeigh;
+        if(imgScale < winScale) {
+            nowWidth = this.ctx.height * imgScale;
+            nowHeigh = this.ctx.height;
+        } else {
+            nowWidth = this.ctx.width;
+            nowHeigh = this.ctx.width / imgScale;
+        }
+        this.ctx.drawImage(bgImg, 0, 0, nowWidth, nowHeigh);
+    };
+    Game.prototype.drawArrow = function(angle, length, x, y) {
+        this.draw();
+        if(length == 0) {
+            return;
+        }
+        var arrowImg = new Image();
+        arrowImg.src = 'images/image.jpeg';
 
-   };
-   Game.prototype.draw = function () {
-       console.log(this.ctx.width)
-       this.ctx.clearRect(0, 0, this.ctx.width, this.ctx.height);
-       this.ctx.fillStyle = '#000';
-       this.ctx.fillRect(0, 0, this.ctx.width, this.ctx.height);
-       var bgImg = new Image();
-       bgImg.src = 'images/tfz.jpg';
-       var imgScale = 288 / 455;
-       var winScale = this.ctx.width / this.ctx.height;
-       var nowWidth, nowHeigh;
-       if(imgScale < winScale) {
-           nowWidth = this.ctx.height * imgScale;
-           nowHeigh = this.ctx.height;
-       } else {
-           nowWidth = this.ctx.width;
-           nowHeigh = this.ctx.width / imgScale;
-       }
-       this.ctx.drawImage(bgImg, 0, 0, nowWidth, nowHeigh);
-   };
-   Game.prototype.resize = function() {
-       this.canvasElem.width = window.outerWidth;
-       this.canvasElem.height = window.outerHeight;
-       this.ctx.width = window.outerWidth;
-       this.ctx.height = window.outerHeight;
-       this.draw();
-   }
-   return Game;
+
+        var rx = x, ry = y;
+        var px = x+length/2, py = y;
+        var radius = rx - px;
+        var dx = rx ;
+        var dy = ry;
+        this.ctx.save();
+        this.ctx.translate(dx, dy);
+        this.ctx.rotate(angle * Math.PI/180);
+        this.ctx.drawImage(arrowImg, 0, -10, length, 20);
+        this.ctx.translate(-dx, -dy);
+
+
+//        ctx.translate(x, y);
+//        this.ctx.rotate(angle*Math.PI/180);
+//        ctx.translate(xpos, ypos);
+//        this.ctx.drawImage(arrowImg, x, y, length, 20);
+        this.ctx.restore();
+    };
+
+    Game.prototype.resize = function() {
+        this.canvasElem.width = window.outerWidth;
+        this.canvasElem.height = window.outerHeight;
+        this.ctx.width = window.outerWidth;
+        this.ctx.height = window.outerHeight;
+    }
+    return Game;
 });
--- slide/main.js
+++ slide/main.js
@@ -1,32 +1,30 @@
 /**
  * Created by rechie on 14-8-7.
  */
-require.config({
-    urlArgs: "bust=" + (new Date()).getTime()
-});
-require(['../core/RC', '../util/MathUtil', '../util/ElemUtil', 'Game'], function (RC, MathUtil, ElemUtil, Game){
-    var game = new Game('gameScreen');
+define(['../core/RC', '../util/MathUtil', '../util/ElemUtil', 'Game'], function (RC, MathUtil, ElemUtil, Game){
+    var game;
     var startPoint = null;
     var endPoint = null;
     var slide = new RC.Slide({
         elemQuery: 'body',
         slideBegin: function(touch){
             startPoint = touch;
-            showArrow(null, null, touch);
+            game.drawArrow(0, 1, startPoint.pageX, startPoint.pageY);
         },
         slideMove: function(touch) {
             endPoint = touch;
             var offsetX = endPoint.pageX - startPoint.pageX;
             var offsetY = endPoint.pageY - startPoint.pageY;
             var length = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2));
-            var angle = 360 - MathUtil.getAngleDirect(startPoint, endPoint);
+            var angle =  360 - MathUtil.getAngleDirect(startPoint, endPoint);
+            game.drawArrow(angle, length, startPoint.pageX, startPoint.pageY);
             showInfo2(angle, length);
-            showArrow(angle, length);
+//            showArrow(angle, length);
         },
         slideEnd: function(touch){
             startPoint = null;
             endPoint = null;
-            hideArrow();
+            game.drawArrow(0, 0, 0, 0);
         }
     });
 
@@ -35,35 +33,11 @@ require(['../core/RC', '../util/MathUtil', '../util/ElemUtil', 'Game'], function
         var ctx = canvasElem.getContext('2d');
         canvasElem.width = window.outerWidth;
         canvasElem.height = window.outerHeight;
-        console.log(ctx)
-        ctx.clearRect(0, 0, ctx.width, ctx.height);
-        ctx.fillStyle = '#000';
-        ctx.fillRect(0, 0, ctx.width, ctx.height);
-        var bgImg = new Image();
-        bgImg.src = 'images/tfz.jpg';
-        ctx.drawImage(bgImg, 0, 0);
+        game = new Game('gameScreen');
     }
 
 
-
-
-    function hideArrow() {
-        var arrow = ElemUtil.query('.arrow');
-        arrow.style.width = 0;
-        arrow.style['-webkit-transform'] = 'rotate(0deg)';
-    }
-
-    function showArrow(angle,length, point) {
-        var arrow = ElemUtil.query('.arrow');
-        if(point) {
-            arrow.style.left = point.pageX + 'px';
-            arrow.style.top = point.pageY + 'px';
-        } else {
-            arrow.style.width = length + 'px';
-            arrow.style['-webkit-transform'] = 'rotate(' + angle + 'deg)';
-        }
-    }
-
+    init();
     function showInfo(htmlStr) {
         ElemUtil.query('.info').innerHTML += (htmlStr + '<br>');
     }
