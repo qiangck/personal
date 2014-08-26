f0bc556cc8c7ff6a38f7f9e04464b045d04dfd26
rechie1985 <lsl1276@gmail.com>
2014-08-26 15:38:34 +0800

    完成游戏滑动判定功能

---
+++ games/chouren/js/app.js
@@ -0,0 +1,210 @@
+/**
+ * Created by rechie on 14-8-26.
+ */
+function resizeContainer() {
+    var widthScale = $(window).width()/320;
+    var heightScale = $(window).height()/480;
+    if(heightScale < widthScale) {
+        $(document.body).addClass('bg-body');
+    }
+    var scale = Math.min(widthScale, heightScale);
+    var realWidth = 320 * scale;
+    var realHeight = 480 * scale;
+    var transformOrigin = [($(window).width()-realWidth)/2, ($(window).height()-realHeight)/2, 0];
+    window.scale = scale;
+    var cssObj = {
+        '-webkit-transform': 'scale(' + scale + ', ' + scale + ')',
+        '-ms-transform': 'scale(' + scale + ', ' + scale + ')',
+        '-o-transform': 'scale(' + scale + ', ' + scale + ')',
+        'transform': 'scale(' + scale + ', ' + scale + ')',
+    };
+    $('.container').css(cssObj);
+    $('.wrap').css({
+        top: '-' + 50/scale + '%',
+        left: '-' + 50/scale + '%'
+    });
+}
+
+var frame1List = [{
+        x:'-5px', 
+        y:'-0px'
+    }, {
+        x:'-130px', 
+        y:'-0px'
+    }, {
+        x:'-256px', 
+        y:'-0px'
+    }, {
+        x:'-382px', 
+        y:'-0px'
+    }, {
+        x:'-507px', 
+        y:'-0px'
+    }, {
+        x:'-632px', 
+        y:'-0px'
+    }, {
+        x:'-5px', 
+        y:'-130px'
+    }, {
+        x:'-130px', 
+        y:'-130px'
+    }, {
+        x:'-256px', 
+        y:'-130px'
+    }, {
+        x:'-382px', 
+        y:'-130px'
+    }, {
+        x:'-507px', 
+        y:'-130px'
+    }, {
+        x:'-632px', 
+        y:'-130px'
+    }, {
+        x:'-5px', 
+        y:'-273px'
+    }, {
+        x:'-130px', 
+        y:'-273px'
+    }, {
+        x:'-256px', 
+        y:'-273px'
+    }, {
+        x:'-382px', 
+        y:'-273px'
+    }, {
+        x:'-507px', 
+        y:'-273px'
+    }, {
+        x:'-632px', 
+        y:'-273px'
+    }, {
+        x:'-5px', 
+        y:'-409px'
+    }, {
+        x:'-130px', 
+        y:'-409px'
+    }, {
+        x:'-256px', 
+        y:'-409px'
+    }, {
+        x:'-382px', 
+        y:'-409px'
+    }, {
+        x:'-507px', 
+        y:'-409px'
+    }, {
+        x:'-632px', 
+        y:'-409px'
+    }
+];
+$(document).ready(function() {
+    resizeContainer();
+    var game = new Game();
+    // 初始化计时器
+    var timer = new Game.Time({
+        add:1,
+        minus: 5,
+        countFunc: Game.View.setTimer,
+        end: function() {
+            console.log('end')
+        }
+    });
+    game.setTimer(timer);
+    $('#play img').bind('click', function(){
+        startGame();
+    });
+    var bOver = false;
+    var index = 0;
+    function startGame() {
+        game.start();
+        var slide = new RC.Slide({ 
+            elemQuery: 'body',
+            slideBegin: function(touch){
+                try{
+                    startPoint = touch;
+                    var element = document.elementFromPoint(touch.pageX, touch.pageY);
+                    if($(element).attr('id') === 'userWrap') {
+                        if(bOver === false) {
+                            console.log('start shake');
+                            console.log(element);
+                            bOver = true;
+                            index ++ ;
+                            console.log('shake time: ' + index);
+                            animate1(0, function(){
+                            });
+                        }
+                    } else {
+                        bOver = false;
+                    }
+                } catch(e) {
+                    alert('begin error: ' + e.message);
+                }
+            },
+            slideMove: function(touch) {
+                try{
+                    var element = document.elementFromPoint(touch.pageX, touch.pageY);
+                    if($(element).attr('id') === 'userWrap') {
+                        if(bOver === false) {
+                            console.log('move shake');
+                            console.log(element);
+                            bOver = true;
+                            index ++ ;
+                            console.log('shake time: ' + index);
+                            animate1(0, function(){
+                            });
+                        }
+                    } else {
+                        bOver = false;
+                    }
+                } catch(e) {
+                    alert('move error: ' + e.message);
+                }
+            },
+            slideEnd: function(touch){
+                try{
+                    bOver = false;
+                } catch(e) {
+                    alert('end error: ' + e.message);
+                }
+            }
+        });
+    }
+
+    var timeId = null;
+    var bAnimate = false;
+    function animate1(index, callback){
+        if(bAnimate == true && index == 0) {
+            return;
+        }
+        if(typeof index !== 'number') {
+            index = 0;
+        }
+        var obj = frame1List[index];
+        $('#userWrap .user').css({
+            'background-position': obj.x + ' ' + obj.y
+        });
+        index ++ ;
+        if(index > frame1List.length - 1) {
+            clearTimeout(timeId);
+            timeId = null;
+            if(typeof callback == 'function') {
+                callback();
+            }
+            bAnimate = false;
+        } else {
+            bAnimate = true;
+            timeId = setTimeout(function(){
+                animate1(index, callback)
+            }, 2000/frame1List.length);
+        }
+    }
+
+    function clearAnimate() {
+        if(timeId) {
+            clearTimeout(timeId);
+            timeId = null
+        }
+    }
+});
---
+++ games/chouren/js/game.js
@@ -0,0 +1,277 @@
+
+
+// 总控制
+var Game = function(options){
+	this._score = 0;
+	this._hitCombo = 0;
+	this._maxCombo = 0;
+	this.init();
+	// 游戏开关
+};
+
+/**
+ * initialize
+ * @return {[type]} [description]
+ */
+Game.prototype.init = function() {
+};
+
+Game.prototype.assetLoadStatus = false;
+Game.prototype.loadImg = function(callback) {
+	var loader = new AssetLoad({
+		success: callback
+	});
+    loader.preLoadImg('images/3.jpg');
+	loader.load();
+
+}
+
+Game.prototype.setScore = function(score) {
+	this._score = score;
+};
+
+Game.prototype.getScore = function() {
+	return this._score;
+};
+
+
+Game.prototype.setTimer = function(timer) {
+	this.timer = timer;
+}
+
+/**
+ * drag rubbish to bins
+ * @param  {[ElementObject]} dragEl [description]
+ * @param  {[ElementObject]} dropEl [description]
+ * @return {[type]}        [description]
+ */
+Game.prototype.dropTo = function(dragEl, dropEl) {
+	if(this.timer.running === false) {
+		return;
+	}
+	if($(dragEl).data('category') === $(dropEl).data('category')) {
+		this.goal(dragEl, dropEl);
+	} else {
+		this.failed(dragEl, dropEl);
+	}
+	this.refreshItem(dragEl.parent());
+};
+
+/**
+ * 成功得分
+ * @return {[type]} [description]
+ */
+Game.prototype.goal = function(dragEl, dropEl) {
+	this.setScore(this._score  + 1);
+	Game.View.setScore(this._score);
+};
+
+
+/**
+ * start the game
+ * @return {[type]} [description]
+ */
+Game.prototype.start = function() {
+	var that = this;
+	if(this.timer.running === true) {
+		console && console.log('游戏正在进行，请勿作弊.')
+		return ;
+	}
+	if(!that.assetLoadStatus) {
+		$('#gameIntro .content').css('visibility', 'hidden');
+		$('#prograss').show();
+		this.loadImg(function(){
+			that.assetLoadStatus = true;
+			go();
+		});
+	} else {
+		console.log('no load');
+		go();
+	}
+	function go(){
+        console.log('go')
+		if(that.timer != null) {
+			$('#gameIntro').hide();
+			$('#gameScreen').show();
+			that.timer.run();
+		}
+	}
+};
+
+Game.prototype.reset = function() {
+	this._score = 0;
+	this._hitCombo = 0;
+	this._maxCombo = 0;
+	Game.View.setScore(this._score);
+};
+
+Game.prototype.replay = function() {
+	console && console.log('重启游戏.');
+	try{
+		this.reset();
+		this.timer.reset();
+		this.start();
+	} catch(e) {
+		console.log(e.stack)
+	}
+};
+
+Game.Utils = Game.Utils || {};
+
+Game.prototype.stop = function() {
+	this.timer.running = false;
+};
+/**
+ * 视图控制
+ * @type {Object}
+ */
+Game.View = {
+	scoreTimeId : null,
+	setTimer: function(current, sum) {
+		var remain = sum - current;
+		$('#timer .numb').html('00:' + (remain<10 ? ('0' + remain ): remain));
+		$('#gameScreen .bottom .progress-wrap .progress').width(Math.floor(remain/sum*100) + '%');
+	},
+	setHitCombo: function(hitCombo) {
+		$('#hitCombo').html(hitCombo);
+	},
+	setScore: function(score) {
+		// 补零方法
+	    var len = score.toString().length;
+	    while(len < 3) {
+	        score = "0" + score;
+	        len++;
+	    }
+		$('#score').html(score);
+	},
+	/**
+	 * 得分状态，true显示成功图标，false显示X
+	 * @param  {[type]} bScored [description]
+	 * @return {[type]}         [description]
+	 */
+	changeStatus: function(target, bScored) {
+		// console.log(target);
+		var $parent = $(target).parent();
+		var childSelector = bScored ? '.scored' : '.failed';
+		$parent.find(childSelector).show();
+		if(Game.View.scoreTimeId !== null) {
+			clearTimeout(Game.View.scoreTimeId);
+		}
+		Game.View.scoreTimeId = setTimeout(function() {
+			Game.View.hideStatus();
+		}, 1000);
+	},
+	hideStatus: function() {
+		$('.scored').hide();
+		$('.failed').hide();
+	},
+	reset: function() {
+		$('#timer .numb').html('01:00');
+	}
+};
+
+
+// 计时器
+Game.Time = function(opt) {
+	this.init(opt);
+};
+
+Game.Time.prototype.init = function(opt) {
+	this.sum = opt.sum || 60;
+	this.add = opt.add;
+	this.minus = opt.minus;
+	this.countFunc = opt.countFunc;
+	this.end = opt.end;
+	this.timeId = null;
+	this.running = false;
+	this.current = 0;
+};
+
+Game.Time.prototype.run = function() {
+	var that = this;
+	this.running = true;
+	that.timeId = setTimeout(function(){
+		if(that.current < that.sum) {
+			that.current ++
+			that.run.call(that);
+			that.countFunc(that.current, that.sum);
+		} else {
+			that.running = false;
+			console && console.log('game over');
+			if(typeof that.end === 'function') {
+				that.end.call(that);
+			}
+		}
+	}, 1000)
+};
+Game.Time.prototype.stop = function() {
+	clearTimeout(this.timeId);
+};	
+
+Game.Time.prototype.reset = function() {
+	this.running = false;
+	clearTimeout(this.timeId);
+	this.current = 0;
+	Game.View.reset();
+};
+Game.Time.prototype.doMinus = function() {
+	this.current += this.minus;
+	if(this.current > this.sum) {
+		this.current = this.sum;
+	}
+	this.countFunc(this.current, this.sum);
+};
+
+/**
+ * 资源加载器
+ * @return {[type]} [description]
+ */
+var AssetLoad = function(conf){
+	this.conf = conf;
+	this.urlList = [];
+	this.sum = 0;
+	this.current = 0;
+};
+AssetLoad.prototype.preLoadImg = function(imgUrl) {
+	if(this.urlList.indexOf(imgUrl) < 0) {
+		this.urlList.push(imgUrl);
+	}
+};	
+AssetLoad.prototype.load = function() {
+	var that = this;
+	that.current = 0;
+	that.urlList.forEach(function(imgUrl, index) {
+		that.loadImage(imgUrl);
+	});
+};
+AssetLoad.prototype.loadImage = function(imgUrl) {
+	var that = this;
+	var image = new Image();
+	image.onload = function(){
+		that.showPrograss();
+	};
+	image.onerror = function(){
+		that.showPrograss();
+	};
+	image.src = imgUrl;
+};
+AssetLoad.prototype.showPrograss = function() {
+	this.current ++;
+	var prograss = this.current / this.urlList.length * 100;
+	var prograssElem = document.querySelector('#prograss span');
+	//console.log(prograssElem);
+	
+	$('.prograss-wrap div').css('width', (prograss|0) + '%');
+//	prograssElem.innerHTML = (prograss|0) + '%';
+	// prograssElem.innerHTML = prograss; 
+	console.log('prograss: ' + (prograss|0));
+
+	if(this.current == this.urlList.length) {
+		this.success();
+	}
+};	
+AssetLoad.prototype.success = function() {
+	if(typeof this.conf.success === 'function') {
+		this.conf.success();
+	}
+};
